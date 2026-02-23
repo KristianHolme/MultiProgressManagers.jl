1f4ceaa4bd470ec42da8496529ff50756b0f64c5
Kristian Holme <holme.kristian@gmail.com>
2026-02-23 19:34:10 +0100

    rename in ext

--- ext/MultiProgressManagersDrillExt.jl
+++ ext/MultiProgressManagersDrillExt.jl
@@ -1,38 +1,38 @@
-module MultiProgressManagersDRiLExt
+module MultiProgressManagersDrillExt
 
 using MultiProgressManagers
 import MultiProgressManagers: create_dril_callback
-import DRiL
+import Drill
 using Distributed
 
-export DRiLWorkerProgressCallback
+export DrillWorkerProgressCallback
 
-mutable struct DRiLWorkerProgressCallback <: DRiL.AbstractCallback
+mutable struct DrillWorkerProgressCallback <: Drill.AbstractCallback
     worker_channel::RemoteChannel{Channel{ProgressMessage}}
 end
 
 
-function DRiL.on_training_start(callback::DRiLWorkerProgressCallback, locals::Dict)
+function Drill.on_training_start(callback::DrillWorkerProgressCallback, locals::Dict)
     worker_channel = callback.worker_channel
     total_steps = locals[:total_steps]
     env = locals[:env]
-    n_envs = DRiL.number_of_envs(env)
+    n_envs = Drill.number_of_envs(env)
     @assert total_steps % n_envs == 0 "total_steps must be divisible by number of environments"
     msg = ProgressStart(myid(), total_steps, "Worker $(myid())")
     put!(worker_channel, msg)
     return true
 end
 
-function DRiL.on_step(callback::DRiLWorkerProgressCallback, locals::Dict)
+function Drill.on_step(callback::DrillWorkerProgressCallback, locals::Dict)
     worker_channel = callback.worker_channel
     env = locals[:env]
-    n_envs = DRiL.number_of_envs(env)
+    n_envs = Drill.number_of_envs(env)
     msg = ProgressStepUpdate(myid(), n_envs, "")
     put!(worker_channel, msg)
     return true
 end
 
-function DRiL.on_training_end(callback::DRiLWorkerProgressCallback, locals::Dict)
+function Drill.on_training_end(callback::DrillWorkerProgressCallback, locals::Dict)
     worker_channel = callback.worker_channel
     put!(worker_channel, ProgressFinished(myid(), "Finished training run!"))
     return true
@@ -41,23 +41,23 @@ end
 """
     create_dril_callback(worker_channel)
 
-Create a DRiL callback for progress tracking.
+Create a Drill callback for progress tracking.
 
 # Arguments
 - `worker_channel::RemoteChannel`: The worker channel from a MultiProgressManager
 
 # Returns
-- `DRiLWorkerProgressCallback`: Callback instance for use with DRiL training
+- `DrillWorkerProgressCallback`: Callback instance for use with Drill training
 
 # Example
 ```julia
-using DRiL
+using Drill
 manager = MultiProgressManager(10)
 callback = create_dril_callback(manager.worker_channel)
 ```
 """
 function create_dril_callback(worker_channel::RemoteChannel{Channel{ProgressMessage}})
-    return DRiLWorkerProgressCallback(worker_channel)
+    return DrillWorkerProgressCallback(worker_channel)
 end
 
-end # module MultiProgressManagersDRiLExt
+end # module MultiProgressManagersDrillExt
