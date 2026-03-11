dcd93a7f1089d5d084c852f1cd089e875396e0da
Kristian Holme <holme.kristian@gmail.com>
2026-03-11 23:18:42 +0100

    Refactor DrillWorkerProgressCallback to use ProgressTask and update progress messaging
    - Changed the structure to include ProgressTask and current step tracking.
    - Updated on_training_start, on_step, and on_training_end functions to utilize the new structure.
    - Modified create_dril_callback to accept a ProgressTask instead of a worker channel.

--- ext/MultiProgressManagersDrillExt.jl
+++ ext/MultiProgressManagersDrillExt.jl
@@ -7,56 +7,62 @@ using Distributed
 export DrillWorkerProgressCallback, create_dril_callback
 
 mutable struct DrillWorkerProgressCallback <: Drill.AbstractCallback
-    worker_channel::RemoteChannel{Channel{ProgressMessage}}
+    task::ProgressTask
+    _current_step::Int
+    _total_steps::Union{Int, Nothing}
 end
 
-
 function Drill.on_training_start(callback::DrillWorkerProgressCallback, locals::Dict)
-    worker_channel = callback.worker_channel
     total_steps = locals[:total_steps]
     env = locals[:env]
     n_envs = Drill.number_of_envs(env)
     @assert total_steps % n_envs == 0 "total_steps must be divisible by number of environments"
-    msg = ProgressStart(myid(), total_steps, "Worker $(myid())")
-    put!(worker_channel, msg)
+    callback._total_steps = total_steps
+    callback._current_step = 0
+    task_number = callback.task.task_number
+    msg = ProgressUpdate(task_number, 0, total_steps, "Worker $(task_number)")
+    put!(callback.task.channel, msg)
     return true
 end
 
 function Drill.on_step(callback::DrillWorkerProgressCallback, locals::Dict)
-    worker_channel = callback.worker_channel
     env = locals[:env]
     n_envs = Drill.number_of_envs(env)
-    msg = ProgressStepUpdate(myid(), n_envs, "")
-    put!(worker_channel, msg)
+    callback._current_step += n_envs
+    task_number = callback.task.task_number
+    total_steps = callback._total_steps
+    msg = ProgressUpdate(task_number, callback._current_step, total_steps, "")
+    put!(callback.task.channel, msg)
     return true
 end
 
 function Drill.on_training_end(callback::DrillWorkerProgressCallback, locals::Dict)
-    worker_channel = callback.worker_channel
-    put!(worker_channel, ProgressFinished(myid(), "Finished training run!"))
+    put!(callback.task.channel, TaskFinished(callback.task.task_number))
     return true
 end
 
 """
-    create_dril_callback(worker_channel)
+    create_dril_callback(task)
 
 Create a Drill callback for progress tracking.
 
 # Arguments
-- `worker_channel::RemoteChannel`: The worker channel from a MultiProgressManager
+- `task::ProgressTask`: The progress task from `get_task(manager, task_number, :remote)`
 
 # Returns
 - `DrillWorkerProgressCallback`: Callback instance for use with Drill training
 
 # Example
 ```julia
+using MultiProgressManagers
 using Drill
-manager = MultiProgressManager(10)
-callback = create_dril_callback(manager.worker_channel)
+manager = create_experiment("my_study", 10; db_path = default_db_path())
+task = get_task(manager, 1, :remote)
+callback = create_dril_callback(task)
 ```
 """
-function create_dril_callback(worker_channel::RemoteChannel{Channel{ProgressMessage}})
-    return DrillWorkerProgressCallback(worker_channel)
+function create_dril_callback(task::ProgressTask)
+    return DrillWorkerProgressCallback(task, 0, nothing)
 end
 
 end # module MultiProgressManagersDrillExt
